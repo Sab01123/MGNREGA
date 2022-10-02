7971ed2f955d354e71c04e9e6385ed7a1338af79
Sabira Farooq <104069112+Sab01123@users.noreply.github.com>
2022-10-02 21:08:36 +0530

    Update BDO.java
    ConflictBDO

--- MGNREGA/src/com/project/bean/BDO.java
+++ MGNREGA/src/com/project/bean/BDO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.project.bean;
 
 public class BDO {
@@ -41,47 +40,3 @@ public String toString() {
 
 
 }
-=======
-package com.project.bean;
-
-public class BDO {
-
-private String BDOname;
-
-private String BDOpassword;
-
-public BDO() {
-	
-}
-
-public BDO(String bDOname, String bDOpassword) {
-	super();
-	BDOname = bDOname;
-	BDOpassword = bDOpassword;
-}
-
-public String getBDOname() {
-	return BDOname;
-}
-
-public void setBDOname(String bDOname) {
-	BDOname = bDOname;
-}
-
-public String getBDOpassword() {
-	return BDOpassword;
-}
-
-public void setBDOpassword(String bDOpassword) {
-	BDOpassword = bDOpassword;
-}
-
-@Override
-public String toString() {
-	return "BDO [BDOname=" + BDOname + ", BDOpassword=" + BDOpassword + "]";
-}
-
-
-
-}
->>>>>>> 8cb2144ddfd0482c344e30e8781c086d363e6fb5
