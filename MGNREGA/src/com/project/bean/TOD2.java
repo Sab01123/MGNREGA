aeb716ff7ec052b9546be51746c980473aa4a7e8
Sabira Farooq <104069112+Sab01123@users.noreply.github.com>
2022-10-02 21:13:25 +0530

    Update TOD2.java
    ConTOD2

--- MGNREGA/src/com/project/bean/TOD2.java
+++ MGNREGA/src/com/project/bean/TOD2.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.project.bean;
 
 public class TOD2 {
@@ -54,78 +53,4 @@ public class TOD2 {
 	public String toString() {
 		return "TOD2 [nOD=" + nOD + ", wedges=" + wedges + "]";
 	}
-
-
-
-
-
-
-
-	
 }
-=======
-package com.project.bean;
-
-public class TOD2 {
-
-	private int nOD;
-	private int wedges;
-	
-	public TOD2() {
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	
-	public TOD2(int nOD, int wedges) {
-		super();
-		this.nOD = nOD;
-		this.wedges = wedges;
-	}
-
-
-
-
-	public int getnOD() {
-		return nOD;
-	}
-
-
-
-
-	public void setnOD(int nOD) {
-		this.nOD = nOD;
-	}
-
-
-
-
-	public int getWedges() {
-		return wedges;
-	}
-
-
-
-
-	public void setWedges(int wedges) {
-		this.wedges = wedges;
-	}
-
-
-
-
-	@Override
-	public String toString() {
-		return "TOD2 [nOD=" + nOD + ", wedges=" + wedges + "]";
-	}
-
-
-
-
-
-
-
-	
-}
->>>>>>> 8cb2144ddfd0482c344e30e8781c086d363e6fb5
