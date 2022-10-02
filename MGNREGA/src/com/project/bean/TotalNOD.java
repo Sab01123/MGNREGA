05a2bfde8cb8fb9359b84b3b63d899d12ae76c77
Sabira Farooq <104069112+Sab01123@users.noreply.github.com>
2022-10-02 21:14:18 +0530

    Update TotalNOD.java
    ConTotalNOD

--- MGNREGA/src/com/project/bean/TotalNOD.java
+++ MGNREGA/src/com/project/bean/TotalNOD.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.project.bean;
 
 public class TotalNOD {
@@ -59,65 +58,3 @@ public class TotalNOD {
 	
 	
 }
-=======
-package com.project.bean;
-
-public class TotalNOD {
-
-	private int NOD;
-	private int wedges;
-	private String empName;
-	private String proName;
-	
-	public TotalNOD() {
-		
-	}
-
-	public TotalNOD(int nOD, int wedges, String empName, String proName) {
-		super();
-		NOD = nOD;
-		this.wedges = wedges;
-		this.empName = empName;
-		this.proName = proName;
-	}
-
-	public int getNOD() {
-		return NOD;
-	}
-
-	public void setNOD(int nOD) {
-		NOD = nOD;
-	}
-
-	public int getWedges() {
-		return wedges;
-	}
-
-	public void setWedges(int wedges) {
-		this.wedges = wedges;
-	}
-
-	public String getEmpName() {
-		return empName;
-	}
-
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-
-	public String getProName() {
-		return proName;
-	}
-
-	public void setProName(String proName) {
-		this.proName = proName;
-	}
-
-	@Override
-	public String toString() {
-		return "TotalNOD [NOD=" + NOD + ", wedges=" + wedges + ", empName=" + empName + ", proName=" + proName + "]";
-	}
-	
-	
-}
->>>>>>> 8cb2144ddfd0482c344e30e8781c086d363e6fb5
