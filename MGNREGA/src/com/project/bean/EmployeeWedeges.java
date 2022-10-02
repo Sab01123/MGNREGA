95b205599fb808a59182b753e23433d717d406f9
Sabira Farooq <104069112+Sab01123@users.noreply.github.com>
2022-10-02 21:10:04 +0530

    Update EmployeeWedeges.java
    ConEmplWages

--- MGNREGA/src/com/project/bean/EmployeeWedeges.java
+++ MGNREGA/src/com/project/bean/EmployeeWedeges.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.project.bean;
 
 public class EmployeeWedeges {
@@ -64,70 +63,3 @@ public class EmployeeWedeges {
 	
 
 }
-=======
-package com.project.bean;
-
-public class EmployeeWedeges {
-	
-	
-	
-	private int empid;
-	private String empName;
-	private String empAddress;
-	private int wedges;
-	
-	public EmployeeWedeges() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public EmployeeWedeges(int empid, String empName, String empAddress, int wedges) {
-		super();
-		this.empid = empid;
-		this.empName = empName;
-		this.empAddress = empAddress;
-		this.wedges = wedges;
-	}
-
-	public int getEmpid() {
-		return empid;
-	}
-
-	public void setEmpid(int empid) {
-		this.empid = empid;
-	}
-
-	public String getEmpName() {
-		return empName;
-	}
-
-	public void setEmpName(String empName) {
-		this.empName = empName;
-	}
-
-	public String getEmpAddress() {
-		return empAddress;
-	}
-
-	public void setEmpAddress(String empAddress) {
-		this.empAddress = empAddress;
-	}
-
-	public int getWedges() {
-		return wedges;
-	}
-
-	public void setWedges(int wedges) {
-		this.wedges = wedges;
-	}
-
-	@Override
-	public String toString() {
-		return "EmployeeWedeges [empid=" + empid + ", empName=" + empName + ", empAddress=" + empAddress + ", wedges="
-				+ wedges + "]";
-	}
-	
-	
-	
-
-}
->>>>>>> 8cb2144ddfd0482c344e30e8781c086d363e6fb5
